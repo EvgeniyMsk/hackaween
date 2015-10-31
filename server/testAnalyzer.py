1f1e617353cf8aafe09edab0d4efd4c32f5a4d1e
DmitriyRD <d.rudenko95@gmail.com>
2015-10-31 22:39:45 +0300

    add test

--- server/testAnalyzer.py
+++ server/testAnalyzer.py
@@ -10,65 +10,33 @@ if __name__ == "__main__":
 
     es = Elasticsearch()
     esc = client.IndicesClient(es)
-
-    #esc.delete(index = "mytemp1")
-    esc.create(index = 'mytemp1', body =  {
-                    "settings": {
-                        "analysis": {
-                            "tokenizer":{
-                  "nGram":{
-                    "type": "nGram",
-                    "min_gram": 4,
-                    "max_gram": 20
-                        }
-                    },
-                  "filter": {
-                    "stopwords_ru": {
-                    "type": "stop",
-                    "stopwords": ['а','без','более','бы','был','была','были','было','быть','в','вам','вас','весь','во','вот','все','всего','всех','вы','где','да','даже','для','до','его','ее','если','есть','еще','же','за','здесь','и','из','или','им','их','к','как','ко','когда','кто','ли','либо','мне','может','мы','на','надо','наш','не','него','нее','нет','ни','них','но','ну','о','об','однако','он','она','они','оно','от','очень','по','под','при','с','со','так','также','такой','там','те','тем','то','того','тоже','той','только','том','ты','у','уже','хотя','чего','чей','чем','что','чтобы','чье','чья','эта','эти','это','я'],
-                    "ignore_case": "true"
-                    },
-                  "custom_word_delimiter": {
-                    "type": "word_delimiter",
-                    # "PowerShot" ⇒ "Power" "Shot", части одного слова становятся отдельными токенами
-                    "generate_word_parts": "true",
-                    "generate_number_parts": "true",  # "500-42" ⇒ "500" "42"
-                    "catenate_words": "true",  # "wi-fi" ⇒ "wifi"
-                    "catenate_numbers": "false",  # "500-42" ⇒ "50042"
-                    "catenate_all": "true",  # "wi-fi-4000" ⇒ "wifi4000"
-                    "split_on_case_change": "true",  # "PowerShot" ⇒ "Power" "Shot"
-                    "preserve_original": "true",  # "500-42" ⇒ "500-42" "500" "42"
-                    "split_on_numerics": "false"  # "j2se" ⇒ "j" "2" "se"
-                  }
-            },
-                "analyzer": {
-                      "customHTMLSnowball": {
-         "type": "custom",
-          "char_filter": [
-            "html_strip"
-          ],
-          "tokenizer": "standard",
-          "filter": [
-            "lowercase",
-            "stop",
-            "snowball"
-          ]
+    esc.delete(index = 'mytemp1')
+    entry_mapping = {
+    'event': {
+        'properties': {
+            'date': {'type': 'integer'},
+            'title': {'type': 'string', "tokenizer" : 'keyword', 'analyzer':"russian"}
         }
-                    }
+    }
+}
 
-                }
-              }
-            }
-            )
+    esc.create('mytemp1')
+    esc.put_mapping(index = 'mytemp1', doc_type='event', body = entry_mapping)
     r = requests.get('http://kudago.com/public-api/v1/events/?fields=title,dates,id&page=1&page_size=100&actual_since={}&actual_until={}&location=spb&categories=concert'.format(time.time(), time.time() + 10713600))
+
     for i in r.json()['results']:
         d = {'title': i['title'], 'date': i['dates'][0]['start']}
-        es.index(index='mytemp', doc_type='event', id=i['id'], body=d)
+        es.index(index='mytemp1', doc_type='event', id=i['id'], body=d)
 
     sset = set()
     result = []
-    for i in temp['music']:
-        res = es.search(index="mytemp", body={'fields': ['title', 'date'], 'query': {'match': {'title': '{}'.format(i['artist'])}}})
+
+    import artist_ranker
+    ranker = artist_ranker.ArtistRanker()
+    top = ranker.rank(temp["music"], 50)
+
+    for i in top:
+        res = es.search(index="mytemp1", body={'fields': ['title', 'date'], 'query': {'match': {'title': '{}'.format(i)}}})
         for item in res['hits']['hits']:
             if not (item['_id'] in sset):
                 result.append({'title' : item['fields']['title'], 'date' : item['fields']['date']})
