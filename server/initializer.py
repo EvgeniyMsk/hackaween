4c8d48d00c24a406a832d2142d2594c81c58f47a
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2015-10-31 14:00:27 +0300

    simple server

--- server/initializer.py
+++ server/initializer.py
@@ -1,11 +1,11 @@
-__author__ = 'surok'
+# -*- coding: utf-8 -*-
 
+import context
 import search_engine
 
 class Initializer(object):
 
-    def __init__(self):
-        self._se = search_engine.SearchEngine()
-
-    def getSE(self):
-        return self._se
+    @staticmethod
+    def get_context():
+        se = search_engine.SearchEngine()
+        return context.Context(se)
